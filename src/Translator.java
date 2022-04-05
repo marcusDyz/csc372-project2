8e0e9c8c1ecdf04d1f7f72c8a2ae92e9aa140a04
dengmarcus <dengmarcus@localhost>
2022-04-05 11:06:50 -0700

    Second update

--- src/Translator.java
+++ src/Translator.java
@@ -6,10 +6,13 @@ public class Translator {
 	
 	public static void main(String[] args) {
 		if (args.length == 0) { // Interactive system
+			// TODO adding explicit parsing feature into Interactive system
 			Scanner scanner = new Scanner(System.in);
+			System.out.print(">> ");
 			String input = scanner.nextLine();
 			while (!input.equals("exit")) { 
 				// TODO Parse the input string and write it in Java code then print the output.
+				System.out.print(">> ");
 				input = scanner.nextLine();
 			}
 			System.out.println("Bye!");
@@ -17,20 +20,30 @@ public class Translator {
 		}else if (args.length == 1) { // Reading file from StdIn
 			try {
 				Scanner scanner = new Scanner(new File(args[0]));
+				String output_filename = (args[0].split(".",0))[0] + ".java";
+				output_filename = output_filename.substring(0, 1).toUpperCase() + output_filename.substring(1);
+				File output = new File(output_filename);
+				if (output.createNewFile()) {
+			        System.out.println("File created: " + output.getName());
+			      } else {
+			        System.out.println("File already exists.");
+			      }
 				while (scanner.hasNextLine()) {
 					// TODO Parse every line and translate to a Java code file.
 				}
-			} catch (FileNotFoundException e) {
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-				System.out.println("Cannot find the file!");
 			}
-		}else { // Error handling
-			System.out.println("Has to be zero or one command line argument!");
+		}else { // Accept command line arguments
+			
 		}
 		
 	}
 	
 	// Private parsing method
+	private void parseCmd(String cmd, File out) {
+		
+	}
 
 }
