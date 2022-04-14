6703378e715dd7c91042e6fbcdb060c43b230278
UAL-Laptop <UAL-Laptop@10.132.194.3>
2022-04-14 16:48:14 -0700

    All printing handled. 4/14/22 4:48 PM

--- src/Translator.java
+++ src/Translator.java
@@ -1,28 +1,34 @@
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//import com.sun.org.apache.xerces.internal.impl.dv.dtd.StringDatatypeValidator;
 
-// Author: YD
 public class Translator {
-	private static Pattern expr = Pattern.compile("^$"); // place holder
-	private static Pattern var_assign = Pattern.compile("^(.+) = (.+)\\$");
-	private static Pattern print_var = Pattern.compile("^print[(](.+)[)]$");
-	private static Pattern print_val = Pattern.compile("^print[(]\"(.+)\"[)]$");
-	private static Pattern if_check = Pattern.compile("^if (.+)$");
-	private static Pattern then_check = Pattern.compile("^then (.+)$");
-	private static Pattern else_check = Pattern.compile("else (.+)$");
-	private static Pattern loop = Pattern.compile("^while (.+) [(]$");
-	private static Pattern comparator = Pattern.compile("^(>|<|>=|<=|==)$");
+	private static Pattern var_assign = Pattern.compile("^(.+) = (.+)$");
+	private static Pattern print_var = Pattern.compile("^print\\((.+)\\)$");
+	private static Pattern print_val = Pattern.compile("^print\\(\"(.+)\"\\)$");
+	private static Pattern if_check = Pattern.compile("^if (.+) \\[$");
+	private static Pattern else_check = Pattern.compile("^else \\[$");
+	private static Pattern loop = Pattern.compile("^while (.+) \\[$");
 	private static Pattern comparative = Pattern.compile("^(.+) (.+) (.+)$");
+	private static Pattern comparator = Pattern.compile("^(>|<|>=|<=|==)$");
+	private static Pattern bool_expr = Pattern.compile("^\\((.+) (.) (.+)\\)$");
+	private static Pattern expr = Pattern.compile("^\\((.+) (.) (.+)\\)$");
+	private static Pattern end_sign = Pattern.compile("^\\]$");
 	private static Pattern intVal = Pattern.compile("^\\d+$");
 	private static Pattern strVal = Pattern.compile("^\"\\w+\"$");
+	private static Pattern and_or = Pattern.compile("^#|\\^$");
 	private static Pattern var = Pattern.compile("^\\w+$");
-	private static Pattern op = Pattern.compile("^[/+/-/*/%/~//]{1}$");
+	private static Pattern op = Pattern.compile("^[+-/%]{1}|\\*$");
 	private static Pattern bool = Pattern.compile("^TRUE|FALSE$");
+	private int end_num = 0; 
+	private static ArrayList<String> variable_list = new ArrayList<String>();
+	
 	
 	public static void main(String[] args) {
 		if (args.length == 0) { // Interactive system
@@ -32,6 +38,7 @@ public class Translator {
 			String input = scanner.nextLine();
 			while (!input.equals("exit")) { 
 				// TODO Parse the input string and write it in Java code then print the output.
+				end_sign(input, true);
 				System.out.print(">> ");
 				input = scanner.nextLine();
 			}
@@ -48,83 +55,209 @@ public class Translator {
 			      } else {
 			        System.out.println("File already exists.");
 			      }
+				FileWriter writer = new FileWriter(output);
 				while (scanner.hasNextLine()) {
 					String cmd = scanner.nextLine();
-					//comparative(cmd, true);
+					parseCmd(cmd, writer);
 					// TODO Parse every line and translate to a Java code file.
 				}
+				writer.close();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} else { // Accept command line arguments
+		}else { // Accept command line arguments
 			
 		}
 		
 	}
 	
 	// Private parsing method
-	private void parseCmd(String cmd, File out) {
+	private static void parseCmd(String cmd, FileWriter out) {
+		String line_result = "";
+		String modified_cmd = cmd.trim();
+		if (varAssign(modified_cmd, false)) {
+			 String[] cur = modified_cmd.split(" ");
+			 if (intVal.matcher(cur[2]).find()) {
+				 line_result += "int " + modified_cmd;
+			 } else if (strVal.matcher(cur[2]).find()) {
+				 line_result += "String " + modified_cmd;
+			 } else if (bool.matcher(cur[2]).find()) {
+				 line_result += "boolean " + cur[0] + cur[1];
+				 if (cur[2] == "TRUE") {
+					 line_result += "true";
+				 } else {
+					 line_result += "false";
+				 }
+			 }
+			line_result += ";";
+		}else if (if_check(modified_cmd, false)) {
+			
+		}else if (else_check(modified_cmd, false)) {
+			 
+		}else if (loop(modified_cmd, false)) {
+			
+		}else if (end_sign(modified_cmd, false)){
 		
+		}else if (print_val(modified_cmd, false)) {
+			line_result += "System.out.println(";
+			String print_val =
+			modified_cmd.substring(modified_cmd.indexOf("(")+1,modified_cmd.indexOf(")"));		
+			line_result += print_val + ");";
+		}else if (print_var(modified_cmd, false)){
+			line_result += "System.out.println(";
+			String print_var = 
+			modified_cmd.substring(modified_cmd.indexOf("(")+1,modified_cmd.indexOf(")"));
+			line_result += print_var + ");";
+		}
+		else {
+			System.out.println("Invalid code detected.");
+			System.exit(0);
+		}
+		System.out.println(line_result);
+		try {
+			out.write(line_result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	private static boolean expr(String cmd, boolean print) {
-		// TODO place holder function.
-		return false;
-	}
-	
-	private boolean varAssign(String cmd, boolean print) {
+	private static boolean varAssign(String cmd, boolean print) {
 		Matcher m = var_assign.matcher(cmd);
 		boolean match = false;
 		if(m.find()) {
 			match = true;
 			match = match && var(m.group(1), print);
 			match = match && val(m.group(2), print);
+			if (print)
+				printMsg(match, "<varAssign>", cmd, "varAssign");
 		}
-		printMsg(match, "<var_assign>", cmd, "variable assignment statement");
 		return match;
 	}
 	
-	public static boolean print_var(String cmd, boolean print) {
+	private static boolean print_var(String cmd, boolean print) {
 		Matcher m = print_var.matcher(cmd);
 		boolean match = false;
-		if(m.find() && print) {
+		if(m.find()) {
 			match = true;
 			match = match && expr(m.group(1), print);
+			if (!(variable_list.contains(m.group(1)))) {
+				System.out.println("COMPILE ERROR: That variable has not been declared.");
+				System.exit(0);
+			}
+			else if (print)
+				printMsg(match, "<print>", cmd, "print statement");
 		}
-		printMsg(match, "<print>", cmd, "print statement");
+		return match;
+	}
+	
+	private static boolean print_val(String cmd, boolean print) {
+		Matcher m = print_val.matcher(cmd);
+		boolean match = false;
+		if(m.find()) {
+			match = true;
+			if (print)
+				printMsg(match, "<print>", cmd, "print statement");
+		}
 		return match;
 	}
 
-	public static boolean print_val(String cmd, boolean print) {
-		Matcher m = print_val.matcher(cmd);
+
+	private static boolean if_check(String cmd, boolean print) {
+		Matcher m = if_check.matcher(cmd);
 		boolean match = false;
-		if(m.find() && print) {
+		if(m.find()) {
 			match = true;
+			match = match && bool_expr(m.group(1), print);
+			//System.out.println(m.group(1));
+			if (print)
+				printMsg(match, "<if>", cmd, "if statement");
 		}
-		printMsg(match, "<print>", cmd, "print statement");
 		return match;
 	}
 	
-	/*private boolean if_check(String cmd, boolean print) {
-		
+	private static boolean else_check(String cmd, boolean print) {
+		Matcher m = else_check.matcher(cmd);
+		boolean match = false;
+		if(m.find()) {
+			match = true;
+			if (print)
+				printMsg(match, "<else>", cmd, "else statement");
+		}
+		return match;
 	}
 	
-	private boolean then_check(String cmd, boolean print) {
-		
+	private static boolean loop(String cmd, boolean print) {
+		Matcher m = loop.matcher(cmd);
+		boolean match = false;
+		if(m.find()) {
+			match = true;
+			match = match && bool_expr(m.group(1), print); // <statement> check
+			if (print)
+				printMsg(match, "<loop>", cmd, "loop");
+		}
+		return match;
+	}
+
+
+	private static boolean bool_expr(String cmd, boolean print) { // TODO
+		boolean match = bool_expr1(cmd, print);
+		if(!match) {
+			Matcher m = bool_expr.matcher(cmd);
+			m.find();
+			match = match && bool_expr(m.group(1),print);
+			match = match && and_or(m.group(2),print);
+			match = match && bool_expr1(m.group(3),print);
+		}
+		if (print)
+			printMsg(match, "<bool_expr>", cmd, "boolean expression");
+		return match;
+	}
+
+	private static boolean bool_expr1(String cmd, boolean print) { // TODO 
+		Matcher m = comparative.matcher(cmd);
+		boolean match = false;
+		if(m.find() && print) {
+			if (comparative(cmd, false) && print) {
+				comparative(cmd, print);
+			}
+		}
+		if (bool.matcher(cmd).find()) {
+			match = true;
+			if (bool(cmd, false) && print) {
+				bool(cmd, print);
+			}
+		}
+		if (var.matcher(cmd).find()){
+			match = true;
+			if (var(cmd, false) && print) {
+				var(cmd, print);
+			}
+		}
+		return match;
 	}
 	
-	private boolean else_check(String cmd, boolean print) {
-		
-	}*/
+	private static boolean expr(String cmd, boolean print) {
+		boolean match = val(cmd, print);
+		if(!match) {
+			Matcher m = expr.matcher(cmd);
+			match = m.find();
+			match = match && expr(m.group(1),print);
+			match = match && op(m.group(2),print);
+			match = match && val(m.group(3),print);
+		}
+		if (print)
+			printMsg(match, "<expr>", cmd, "expression");
+		return match;
+	}
 	
 	private static boolean comparator(String cmd, boolean print) {
 		Matcher m = comparator.matcher(cmd);
 		boolean match = false;
-		if (m.find()) {
-			match = true;
-		}
-		printMsg(match, "<comparator>", cmd, "comparator");
+		match = m.find();
+		if (print)
+			printMsg(match, "<comparator>", cmd, "comparator");
 		return match;
 	}
 	
@@ -133,52 +266,78 @@ public class Translator {
 		boolean match = false;
 		if (m.find()) {
 			match = true;
+			match = match && expr(m.group(1), print);
 			match = match && comparator(m.group(2),print);
+			match = match && expr(m.group(3), print);
+			if (print)
+				printMsg(match, "<comparative>", cmd, "comparative");
 		}
-		printMsg(match, "<comparative>", cmd, "comparative statement");
 		return match;
 	}
 	
+	private static boolean and_or(String cmd, boolean print) {
+		Matcher m = and_or.matcher(cmd);
+		boolean match = false;
+		match = m.find();
+		if (print)
+			printMsg(match, "<and_or>", cmd, "And|Or");
+		return match;
+	}
+	
+	private static boolean end_sign(String cmd, boolean print) {
+		Matcher m = end_sign.matcher(cmd);
+		boolean match = m.find();
+		if (print) 
+			printMsg(match, "<end_sign>", cmd, "End sign");
+		return match;
+	}
 	
 	private static boolean var(String cmd, boolean print) {
 		Matcher m = var.matcher(cmd);
 		boolean match = m.find();
+		if (match) variable_list.add(cmd);
 		if (print) 
 			printMsg(match, "<var>", cmd, "variable");
 		return match;
 	}
 	
-	private boolean val(String cmd, boolean print) {
+	private static boolean val(String cmd, boolean print) {
 		Matcher m = intVal.matcher(cmd);
 		boolean match = m.find();
-		String result = "";
 		if(match && print) {
 			printMsg(match, "<int>", cmd, "integer");
-		}else if (strVal.matcher(cmd).find() && print) {
-			printMsg(match, "<String>", cmd, "string");
+		}else if (strVal.matcher(cmd).find()) {
+			match = true;
+			if (print)
+				printMsg(match, "<String>", cmd, "string");
+		}else if (bool.matcher(cmd).find()){
+			match = true;
+			if (print)
+				printMsg(match, "<bool>", cmd, "boolean");
 		}else {
-			m = bool.matcher(cmd);
+			m = var.matcher(cmd);
 			match = m.find();
 			if(match && print)
-				printMsg(match, "<bool>", cmd, "boolean");
-			else {
-				m = var.matcher(cmd);
-				match = m.find();
-				if(match && print)
-					printMsg(match, "<var>", cmd, "variable");
-			}
+				printMsg(match, "<var>", cmd, "variable");
 		}
-		printMsg(match, "<val>", cmd, "value");
 		return match;
 	}
 	
 	private static boolean op(String cmd, boolean print) {
 		Matcher m = op.matcher(cmd);
 		boolean match = false;
-		if (m.find()) {
-			match = true;
-		}
-		printMsg(match, "<op>", cmd, "operator");
+		match = m.find();
+		if (print)
+			printMsg(match, "<op>", cmd, "operator");
+		return match;
+	}
+	
+	private static boolean bool(String cmd, boolean print) {
+		Matcher m = bool.matcher(cmd);
+		boolean match = false;
+		match = m.find();
+		if (print)
+			printMsg(match, "<bool>", cmd, "boolean");
 		return match;
 	}
 		
